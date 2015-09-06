856727b64d18329e8bb403ac73e3cd6bc04cdf2c
liuxuan <liuxuan@ucfgroup.com>
2015-09-06 11:25:58 +0800

    Initial Commit

---
+++ taxcalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taxcalculator
+//
+//  Created by LiuX on 15/9/6.
+//  Copyright © 2015年 Maitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ taxcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  taxcalculator
+//
+//  Created by LiuX on 15/9/6.
+//  Copyright © 2015年 Maitang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
